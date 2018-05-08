2685c9dae0f7edc37ce803188fc9d5711a1503db
Kyle Treman <jkt290@nyu.edu>
2018-05-08 18:10:07 -0400

    inital project config and bot setup

---
+++ main.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('NDQzNTI3NDMxMzY2OTAxNzc3.DdOsKA.XJa0TFUkGSwmsQiLV633ouf7dwM');
