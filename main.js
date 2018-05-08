d0426e91d825fe4b6e7524981490b2c7207627e8
Kyle Treman <jkt290@nyu.edu>
2018-05-08 18:18:54 -0400

    Simple ping pong response

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
+// require the discord.js module
 const Discord = require('discord.js');
+
+// create a new Discord client
 const client = new Discord.Client();
 
+// when the client is ready, run this code
+// this event will trigger whenever your bot:
+// - finishes logging in
+// - reconnects after disconnecting
 client.on('ready', () => {
-    console.log('Ready!');
+  console.log('Ready!');
 });
 
+client.on('message', message => {
+  if (message.content === '!ping') {
+    // send back "Pong." to the channel the message was sent in
+    message.channel.send('Pong.');
+  }
+});
+
+// login to Discord with your app's token
 client.login('NDQzNTI3NDMxMzY2OTAxNzc3.DdOsKA.XJa0TFUkGSwmsQiLV633ouf7dwM');
